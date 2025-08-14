435e048cc2006c1f1b3700b507c972cfd5119ff8
Ramalakshmi <iramalakshmiramya@gmail.com>
2025-08-15 00:11:33 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET || "access";
 
 const app = express();
 
@@ -10,9 +11,25 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (!req.session || !req.session.authorization) {
+      return res.status(403).json({ message: "User not logged in" });
+    }
+  
+    const token = req.session.authorization.accessToken;
+    if (!token) {
+      return res.status(403).json({ message: "Access token missing" });
+    }
+  
+    try {
+      const user = jwt.verify(token, ACCESS_TOKEN_SECRET);
+      req.user = user;
+      return next();
+    } catch (err) {
+      return res.status(403).json({ message: "User not authenticated" });
+    }
+  });
+  
  
 const PORT =5000;
 
